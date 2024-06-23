2fb36fb2316e01fb3e3f334fa46522036090b3da
Yoseph <yoseph.samcaha@gmail.com>
2024-06-23 14:01:21 +0100

    Refactoring random variable generator

--- milestone_2.py
+++ milestone_2.py
@@ -4,14 +4,14 @@ import random
 word_list = ["Bananas","Pineapple","Blueberries","Strawberries","Peaches"]
 
 #chooses a random word using "choice" from the list passed in as arg
-word = random.choice(word_list)
+random_word = random.choice(word_list)
 
 
 #Random letter input with conditional checks 
-guess = input("Please enter a single letter: ")
+input_guess = input("Please enter a single letter: ")
 
 #Checking if the guess is 1 long and is also in the alhpabet
-if len(guess) == 1 and guess.isalpha():
+if len(input_guess) == 1 and input_guess.isalpha():
     print("Good Guess!")
 
 else:
